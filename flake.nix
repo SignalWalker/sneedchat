8ace6e588f77366e702f4812b9dc2fbe1a2dd53d
Ash Walker <ashurstwalker@gmail.com>
2024-02-11 12:16:03 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    alejandra = {
+      url = "github:kamadorueda/alejandra";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }:
+    with builtins; let
+      std = nixpkgs.lib;
+      systems = ["x86_64-linux"];
+      nixpkgsFor = std.genAttrs systems (system:
+        import nixpkgs {
+          localSystem = builtins.currentSystem or system;
+          crossSystem = system;
+          overlays = [];
+        });
+    in {
+      formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
+      devShells = std.genAttrs systems (system: let
+        pkgs = nixpkgsFor.${system};
+      in {
+        default = pkgs.mkShell (let
+          python = pkgs.python311.withPackages (ps:
+            with ps; [
+              cryptography
+              stem
+            ]);
+        in {
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+          buildInputs = [
+            pkgs.sqlite
+            pkgs.openssl
+            pkgs.zlib
+          ];
+          packages = [
+            # python
+            # pkgs.tor
+          ];
+          env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.sqlite.dev}/lib/pkgconfig";
+
+          LD_LIBRARY_PATH = std.concatStringsSep ":" ["${pkgs.sqlite.out}/lib" "${pkgs.openssl.out}/lib"];
+        });
+      });
+    };
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+pub struct SneedChat {}
+
+impl Application for SneedChat {}
+
+fn main() {}
